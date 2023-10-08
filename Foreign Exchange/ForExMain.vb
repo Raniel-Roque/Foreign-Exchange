8ad09eb8d96f08e10270c9a12dc33c3418082717
Raniel-Roque <raniel.roque0509@gmail.com>
2023-10-08 19:31:19 +0800

    Add project files.

---
+++ Foreign Exchange/ForExMain.vb
@@ -0,0 +1,20 @@
+﻿Public Class ForExMain
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Australian Dollar (AUD)
+        'Bahraini Dinar(BHD)
+        'Chinese Yuan(CNY)
+        'Euro(EUR)
+        'Indian Rupee(INR)
+        'Japanese Yen(JPY)
+        'Kuwaiti Dinar(KWD)
+        'Omani Rial(OMR)
+        'Philippine Peso(PHP)
+        'US Dollar(USD)
+
+        '08/10/2023
+        Dim ExchangeRateToUSD() As Double = {0.64, 2.67, 0.14, 1.06, 0.012, 0.0067, 0.31, 2.61, 0.018, 1}
+        Dim ExchangeRateFromUSD() As Double = {1.57, 0.38, 7.19, 0.94, 83.12, 149.22, 3.25, 0.38, 56.7, 1}
+
+        TextBox2.Text = (TextBox1.Text * ExchangeRateToUSD(ComboBox1.SelectedIndex)) * ExchangeRateFromUSD(ComboBox2.SelectedIndex)
+    End Sub
+End Class
---
+++ Foreign Exchange/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Foreign Exchange")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("Foreign Exchange")>
+<Assembly: AssemblyCopyright("Copyright © HP 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("814a5e30-d7f9-4300-b668-b8fa95f062d7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
